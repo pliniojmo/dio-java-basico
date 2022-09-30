eb760a022f5a5e4d51537f554ed4bd39b1d4a8a2
Plínio José Mendes de Oliveira <73654431+pliniojmo@users.noreply.github.com>
2022-09-30 14:05:32 -0300

    Primeira aula

---
+++ iFood-JavaBeginners/src/aula01/TiposVariaveis.java
@@ -0,0 +1,15 @@
+package aula01;
+
+import java.util.Scanner;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+
+
+
+
+        sc.close();
+    }
+}
