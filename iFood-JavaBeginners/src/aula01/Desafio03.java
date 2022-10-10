71b41024914ae77884c6475e459b07e323c6465b
Plínio José Mendes de Oliveira <73654431+pliniojmo@users.noreply.github.com>
2022-10-10 10:58:25 -0300

    Primeiro desafio

---
+++ iFood-JavaBeginners/src/aula01/Desafio03.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Desafio03 {
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+
+        //TODO: Imprima o somatório de N (utilize o método "somatorio").
+        
+        int n = numero.nextInt();
+
+        System.out.println(somatorio(n));
+        
+    }
+    static int somatorio(int numero) {
+        if (numero == 0) {
+            return 0;
+        } else {
+            return numero + somatorio(numero - 1);
+        }
+    }
+}
